cc59d66bced7f0ac7a0927bc32fc2b95882ac9e3
Yubin Kuang <yubin.kse@gmail.com>
2014-11-19 15:20:06 +0100

    context: set sensor database path

--- opensfm/context.py
+++ opensfm/context.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 TWO_VIEW_RECONSTRUCTION = 'lib/build/two_view_reconstruction'
+P3PF = 'lib/build/bin/p3pf_example'
 BUNDLE = 'lib/build/bundle'
-
+SENSOR = 'share/opensfm/sensor_data.json'
