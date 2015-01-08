6ad708d7fb6f8170837332a78e758c1a36841086
Yubin Kuang <yubin.kse@gmail.com>
2015-01-08 03:10:55 +0100

    context: using absolute path

--- opensfm/context.py
+++ opensfm/context.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-
-TWO_VIEW_RECONSTRUCTION = 'lib/build/two_view_reconstruction'
-BUNDLE = 'lib/build/bundle'
-SENSOR = 'share/opensfm/sensor_data.json'
-AKAZE = 'lib/build/akaze'
+import os
+abspath = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+TWO_VIEW_RECONSTRUCTION = os.path.join(abspath, 'lib/build/two_view_reconstruction')
+BUNDLE = os.path.join(abspath, 'lib/build/bundle')
+SENSOR = os.path.join(abspath,  'share/opensfm/sensor_data.json')
+AKAZE = os.path.join(abspath,  'lib/build/akaze')
