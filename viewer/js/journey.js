de9196266ee6027410ead46be0d840809496ca78
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2015-02-15 17:43:24 +0100

    Creating straight line segment curve.
    Straight line segment curve created from line geometry points.

--- viewer/js/journey.js
+++ viewer/js/journey.js
@@ -313,6 +313,33 @@ var Journey = (function () {
     return Journey;
 })();
 
+LineSegmentCurve = THREE.Curve.create(
+
+	function (points) {
+
+		this.points = (points == undefined) ? [] : points;
+
+	},
+
+	function (t) {
+
+		var points = this.points;
+		var point = (points.length - 1) * t;
+
+		var intPoint = Math.floor(point);
+		var weight = point - intPoint;
+
+		var point1 = points[intPoint];
+		var point2 = points[intPoint > points.length - 2 ? points.length - 1 : intPoint + 1 ];
+
+		var vector = new THREE.Vector3();
+		vector.copy(point1).lerp(point2, weight);
+
+		return vector;
+
+	}
+);
+
 var JourneyWrapper = (function ($) {
 
     /**
@@ -413,7 +440,10 @@ var JourneyWrapper = (function ($) {
             geometry.vertices.push(new THREE.Vector3(oc.x, oc.y, oc.z));
         }
 
-        return new THREE.Line(geometry, material);
+        var line = new THREE.Line(geometry, material);
+        var lineCurve = new LineSegmentCurve(geometry.vertices);
+
+        return { 'line': line, 'lineCurve': lineCurve}
     }
 
     /**
@@ -463,7 +493,8 @@ var JourneyWrapper = (function ($) {
         this.hidePath();
 
         var path = this.journey.shortestPath(selectedCamera.shot_id, this.destination).path;
-        this.line = createLineGeometry(this.shots, path);
+        var lineGeometry = createLineGeometry(this.shots, path);
+        this.line = lineGeometry.line;
         this.line.name = "shortestPath"
         scene.add(this.line);
         render();
