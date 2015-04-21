743daf4ae314cde89da8fb2ffe10376f3e0fd0da
Pau Gargallo <pau.gargallo@gmail.com>
2015-04-21 21:35:54 +0200

    Prevent divide by 0 while meshing

--- opensfm/mesh.py
+++ opensfm/mesh.py
@@ -36,7 +36,8 @@ def triangle_mesh(r, graph, data):
                             depths[i] += reconstruction.camera_coordinates(cam, shot, vertices[j])[2]
                             sums[i] += 1
         for i in range(4):
-            vertices[i] = reconstruction.back_project(cam, shot, pixels[i], depths[i] / sums[i]).tolist()
+            w = sums[i] or 50.0
+            vertices[i] = reconstruction.back_project(cam, shot, pixels[i], depths[i] / w).tolist()
 
         faces = tri.simplices.tolist()
         r['shots'][shot_id]['vertices'] = vertices
