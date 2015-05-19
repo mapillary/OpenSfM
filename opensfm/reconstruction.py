mapillary/OpenSfM

commit 3d915dd0846f4c25942ba996129c3ee6686d258d

Using camera distortion for epipolar geo plot.
Undistorting corresponding points before calculating epipolar lines. Distorting epipolar line points to create epipolar curves. Distorting epipole.
Refactoring reconstruction.reproject function to be able to reuse the distort code in plot_track.
Improving line points calculation to include as many curve points as possible in the plot.

opensfm/reconstruction.py
@@ -224,6 +224,24 @@ def back_project(camera, shot, pixel, depth):
     return np.linalg.solve(A, b)
 
 
+def distort(camera, x, y):
+    """ Distorts a point (x, y) using the distortion coefficients of the camera.
+    :param camera: The camera.
+    :param x: The x coordinate.
+    :param y: The y coordinate.
+    :return: The distorted x and y coordinates.
+    """
+    l1 = camera.get('k1', 0.0)
+    l2 = camera.get('k2', 0.0)
+    r2 = x * x + y * y
+    distortion = 1.0 + r2 * (l1 + l2 * r2)
+
+    x_distort = distortion * x
+    y_distort = distortion * y
+
+    return x_distort, y_distort
+
+
 def reproject(camera, shot, point):
     ''' Reproject 3D point onto image plane given a camera
     '''
@@ -232,15 +250,13 @@ def reproject(camera, shot, point):
     xp = p[0] / p[2]
     yp = p[1] / p[2]
 
-    l1 = camera.get('k1', 0.0)
-    l2 = camera.get('k2', 0.0)
-    r2 = xp * xp + yp * yp
-    distortion = 1.0 + r2  * (l1 + l2  * r2)
+    x_unnormalized = camera['focal'] * xp
+    y_unnormalized = camera['focal'] * yp
 
-    x_reproject = camera['focal'] * distortion * xp
-    y_reproject = camera['focal'] * distortion * yp
+    x_reproject, y_reproject = distort(camera, x_unnormalized, y_unnormalized)
 
     return np.array([x_reproject, y_reproject])
+
 
 def single_reprojection_error(camera, shot, point, observation):
     ''' Reprojection error of a single points

bin/plot_track
@@ -126,18 +126,6 @@ def image_plane_corners(oc, KR_inv, width, height):
     return np.add(np.resize(oc, (3, 1)), corners)
 
 
-def camera_matrix(im, reconstruction):
-    """ Retrieves the camera matrix for an image.
-
-    :param im: The image name.
-    :param reconstruction: A reconstruction.
-    :return: The camera matrix.
-    """
-    shot = reconstruction['shots'][im]
-    camera = reconstruction['cameras'][shot['camera']]
-    return reconstruct.projection_matrix(camera, shot)
-
-
 def epipole(Pi, Cj):
     """ Calculates the epipole for another camera.
 
@@ -180,17 +168,23 @@ def epipolar_line(F, x):
     return np.dot(F, x)
 
 
-def line_equation_points(l, x0=-.5, xn=.5, N=2):
+def line_equation_points(l, xy_min=-1., xy_max=1., n=200):
     """ Calculates points on a line.
 
     :param l: Line equation
-    :param x0: Start x value.
-    :param xn: End x value.
-    :param N: Number of points.
+    :param xy_min: Smallest value.
+    :param xy_max: Largest value.
+    :param n: Number of points.
     :return: An array of points that lie on the line.
     """
-    x = np.linspace(x0, xn, N)
-    y = (-l[0] * x - l[2]) / l[1]
+
+    # Create linear space points along axis with most slope with respect to the other.
+    if abs(l[1]) < 1.e-4 or np.abs(l[0] / l[1]) > 1:
+        y = np.linspace(xy_min, xy_max, n)
+        x = (-l[1] * y - l[2]) / l[0]
+    else:
+        x = np.linspace(xy_min, xy_max, n)
+        y = (-l[0] * x - l[2]) / l[1]
 
     x = np.resize(x, (x.shape[0], 1))
     y = np.resize(y, (y.shape[0], 1))
@@ -250,6 +244,33 @@ def find_reconstruction(images, data):
     return reconstruction, rec_images
 
 
+def distort_points(camera, x):
+    """ Distorts a points using the distortion coefficients of the camera.
+    :param camera: The camera.
+    :param x: The points.
+    :return: Array of distorted points.
+    """
+    result = []
+
+    for point in x:
+        x_distort, y_distort = reconstruct.distort(camera, point[0], point[1])
+        result.append(np.array([x_distort, y_distort]))
+
+    return np.array(result)
+
+
+def shot_camera(im, reconstruction):
+    """ Retrieves the shot and camera for an image in a reconstruction.
+    :param im: The image name.
+    :param reconstruction: The reconstruction.
+    :return: The shot and the camera for the image.
+    """
+    shot = reconstruction['shots'][im]
+    camera = reconstruction['cameras'][shot['camera']]
+
+    return shot, camera
+
+
 def chunks(l, n):
     """ Separates a list in smaller chunks of items.
 
@@ -261,11 +282,10 @@ def chunks(l, n):
         yield l[i:i+n]
 
 
-def plot_image_feature(plot, im, x, reconstruction, point, fraction, data):
+def plot_image_feature(plot, im, x, reconstruction, point, fraction):
     xh = multiview.homogeneous(x)
 
-    shot = reconstruction['shots'][im]
-    camera = reconstruction['cameras'][shot['camera']]
+    shot, camera = shot_camera(im, reconstruction)
     P = reconstruct.projection_matrix(camera, shot)
     KR_inv = np.linalg.inv(P[:, :3])
 
@@ -322,7 +342,7 @@ def plot_track_ray(track, data):
     points = []
     for index, im in enumerate(rec_images):
         fraction = 1 if len(rec_images) == 1 else float(index) / (len(rec_images) - 1)
-        oc = plot_image_feature(plot, im, np.array(images[im]['feature']), reconstruction, point, fraction, data)
+        oc = plot_image_feature(plot, im, np.array(images[im]['feature']), reconstruction, point, fraction)
         points.append(oc)
 
     if point is not None:
@@ -349,27 +369,42 @@ def plot_track_ray(track, data):
     pl.show()
 
 
-def plot_epipolar_line(subplot, im1, im2, im1_array, im2_array, x1, x2, reconstruction):
-    P1 = camera_matrix(im1, reconstruction)
-    P2 = camera_matrix(im2, reconstruction)
+def plot_epipolar_geo(subplot, im1, im2, im1_array, im2_array, x1, x2, reconstruction):
+    shot1, camera1 = shot_camera(im1, reconstruction)
+    shot2, camera2 = shot_camera(im2, reconstruction)
 
-    C1 = multiview.homogeneous(reconstruct.optical_center(reconstruction['shots'][im1]))
-    C2 = multiview.homogeneous(reconstruct.optical_center(reconstruction['shots'][im2]))
+    P1 = reconstruct.projection_matrix(camera1, shot1)
+    P2 = reconstruct.projection_matrix(camera2, shot2)
+
+    C1 = multiview.homogeneous(reconstruct.optical_center(shot1))
+    C2 = multiview.homogeneous(reconstruct.optical_center(shot2))
 
     F = fundamental_matrix(P1, P2, C1)
 
-    l1 = epipolar_line(F.T, multiview.homogeneous(x2))
-    l2 = epipolar_line(F, multiview.homogeneous(x1))
+    # Undistort points before calculating epipolar line.
+    x1u = np.squeeze(multiview.undistort_points(camera1, np.reshape(x1, (2, 1))))
+    x2u = np.squeeze(multiview.undistort_points(camera2, np.reshape(x2, (2, 1))))
+
+    l1 = epipolar_line(F.T, multiview.homogeneous(x2u))
+    l2 = epipolar_line(F, multiview.homogeneous(x1u))
 
     lp1 = line_equation_points(l1)
     lp2 = line_equation_points(l2)
 
+    # Distort epipolar line points to transform them to epipolar curve points.
+    lp1d = distort_points(camera1, lp1)
+    lp2d = distort_points(camera2, lp2)
+
     e1 = epipole(P1, C2)
     e2 = epipole(P2, C1)
 
+    # Distort epipole.
+    e1d = np.array(reconstruct.distort(camera1, e1[0], e1[1]))
+    e2d = np.array(reconstruct.distort(camera2, e2[0], e2[1]))
+
     plot_points(subplot, im1_array, im2_array, np.resize(x1, (1, 2)), np.resize(x2, (1, 2)), 'oc', 'oc')
-    plot_points(subplot, im1_array, im2_array, np.resize(e1[:2], (1, 2)), np.resize(e2[:2], (1, 2)), 'or', 'or')
-    plot_points(subplot, im1_array, im2_array, lp1, lp2, 'r', 'r')
+    plot_points(subplot, im1_array, im2_array, np.resize(e1d[:2], (1, 2)), np.resize(e2d[:2], (1, 2)), 'or', 'or')
+    plot_points(subplot, im1_array, im2_array, lp1d, lp2d, 'r', 'r')
 
 
 def plot_epipolar_lines(track, data, images=[], save_figs=False, single_column=False):
@@ -427,7 +462,7 @@ def plot_epipolar_lines(track, data, images=[], save_figs=False, single_column=F
             x1 = np.array(track_ims[im1]['feature'])
             x2 = np.array(track_ims[im2]['feature'])
 
-            plot_epipolar_line(subplot, im1, im2, im1_array, im2_array, x1, x2, reconstruction)
+            plot_epipolar_geo(subplot, im1, im2, im1_array, im2_array, x1, x2, reconstruction)
 
         fig.subplots_adjust(left=0.01, bottom=0.01, right=0.99, top=0.93 if save_figs else 0.91,
                             wspace=0.04, hspace=0.14)

commit d24613a9783948f65c9f36436d718b7d91576200

Fixing distortion bug introduced in commit 3d915dd.
Distorting normalized image coordinates in focal length units instead of incorrectly distorting 'undistorted image coordinates'. The implementation was correct up until commit 3d915dd. Error pointed out by @paulinus .

opensfm/reconstruction.py
@@ -224,22 +224,23 @@ def back_project(camera, shot, pixel, depth):
     return np.linalg.solve(A, b)
 
 
-def distort(camera, x, y):
-    """ Distorts a point (x, y) using the distortion coefficients of the camera.
+def distort(camera, x_normalized, y_normalized):
+    """ Distorts a normalized point (x, y) with coordinates in focal length units using the
+        distortion coefficients of the camera.
 
     :param camera: The camera.
-    :param x: The x coordinate.
-    :param y: The y coordinate.
-    :return: The distorted x and y coordinates.
+    :param x_normalized: The normalized x coordinate in focal length units.
+    :param y_normalized: The normalized y coordinate in focal length units.
+    :return: The distorted x and y coordinates in focal length units.
     """
 
     k1 = camera.get('k1', 0.0)
     k2 = camera.get('k2', 0.0)
-    r2 = x * x + y * y
+    r2 = x_normalized * x_normalized + y_normalized * y_normalized
     distortion = 1.0 + r2 * (k1 + k2 * r2)
 
-    x_distort = distortion * x
-    y_distort = distortion * y
+    x_distort = distortion * x_normalized
+    y_distort = distortion * y_normalized
 
     return x_distort, y_distort
 
@@ -249,10 +250,13 @@ def reproject(camera, shot, point):
     '''
     p = rotate(shot['rotation'], point['coordinates'])
     p += shot['translation']
-    xp = camera['focal'] * p[0] / p[2]
-    yp = camera['focal'] * p[1] / p[2]
+    xp = p[0] / p[2]
+    yp = p[1] / p[2]
 
-    x_reproject, y_reproject = distort(camera, xp, yp)
+    x_distort, y_distort = distort(camera, xp, yp)
+
+    x_reproject = camera['focal'] * x_distort
+    y_reproject = camera['focal'] * y_distort
 
     return np.array([x_reproject, y_reproject])
 