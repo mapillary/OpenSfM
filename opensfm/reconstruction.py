f46112d47bf91ca436f24cb8ffa7fcbaa73e5c25
Yubin Kuang <yubin.kse@gmail.com>
2015-05-20 21:09:36 +0200

    Set default orientation for invalid orientation

--- opensfm/reconstruction.py
+++ opensfm/reconstruction.py
@@ -508,7 +508,8 @@ def get_horitzontal_and_vertical_directions(R, orientation):
         return -R[1, :], -R[0, :], -R[2, :]
     if orientation == 8:
         return -R[1, :],  R[0, :],  R[2, :]
-    print 'ERROR unknown orientation', orientation
+    print 'ERROR unknown orientation {0} - Assuming orientation 1 instead.'.format(orientation)
+    return  R[0, :],  R[1, :],  R[2, :]
 
 
 def align_reconstruction(reconstruction, config):
