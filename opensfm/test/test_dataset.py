c07c689c6e0d18d3b6b66878d02ab90f2b72336d
Pau Gargallo <pau.gargallo@gmail.com>
2015-11-09 13:38:17 +0100

    Add sift i/o test

---
+++ opensfm/test/test_dataset.py
@@ -0,0 +1,32 @@
+import os
+
+import numpy as np
+
+import opensfm.dataset
+
+
+def create_berlin_test_folder(tmpdir):
+    path = str(tmpdir.mkdir('berlin'))
+    os.symlink(os.path.abspath('data/berlin/images'),
+        os.path.join(path, 'images'))
+    return opensfm.dataset.DataSet(path)
+
+
+def test_dataset_load_features_sift(tmpdir):
+    data = create_berlin_test_folder(tmpdir)
+
+    assert len(data.images()) == 3
+
+    data.config['feature_type'] = 'SIFT'
+
+    image = data.images()[0]
+    points = np.random.random((3,4))
+    descriptors = np.random.random((128,4))
+    colors = np.random.random((3,4))
+    data.save_features(image, points, descriptors, colors)
+
+    p, d, c = data.load_features(image)
+
+    assert np.allclose(p, points)
+    assert np.allclose(d, descriptors)
+    assert np.allclose(c, colors)
