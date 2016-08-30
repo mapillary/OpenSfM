62522c93db1d815889b97cbb845523fa4cf75c36
Pau Gargallo <pau.gargallo@gmail.com>
2016-08-30 09:54:22 +0200

    Compute correlation scores

--- opensfm/src/depthmap.cc
+++ opensfm/src/depthmap.cc
@@ -60,7 +60,66 @@ class DepthmapEstimator {
     cv::Matx33d H = PlaneInducedHomography(Ks_[0], Rs_[0], ts_[0],
                                            Ks_[other], Rs_[other], ts_[other],
                                            cv::Matx31d(0, 0, -1 / depth));
-    return 0;
+
+    int patch_size = 7;
+    int hpz = (patch_size - 1) / 2;
+    float patch1[patch_size * patch_size];
+    float patch2[patch_size * patch_size];
+    int counter = 0;
+    for (int u = -hpz; u <= hpz; ++u) {
+      for (int v = -hpz; v <= hpz; ++v) {
+        patch1[counter] = images_[0].at<unsigned char>(i + u, j + v);
+        float x2, y2;
+        ApplyHomography(H, j + v, i + u, &x2, &y2);
+        patch2[counter] = LinearInterpolation(images_[other], y2, x2);
+        counter++;
+      }
+    }
+    return NormalizedCrossCorrelation(patch1, patch2, patch_size * patch_size);
+  }
+
+  float LinearInterpolation(const cv::Mat &image, float y, float x) {
+    int ix = int(x);
+    int iy = int(y);
+    float dx = x - ix;
+    float dy = y - iy;
+    float im00 = image.at<unsigned char>(iy, ix);
+    float im01 = image.at<unsigned char>(iy, ix + 1);
+    float im10 = image.at<unsigned char>(iy + 1, ix);
+    float im11 = image.at<unsigned char>(iy + 1, ix + 1);
+    float im0 = (1 - dx) * im00 + dx * im01;
+    float im1 = (1 - dx) * im10 + dx * im11;
+    return (1 - dy) * im0 + dy * im1;
+  }
+
+  float NormalizedCrossCorrelation(float *x, float *y, int n) {
+    float sumx = 0;
+    float sumx2 = 0;
+    float sumy = 0;
+    float sumy2 = 0;
+    for (int i = 0; i < n; ++i) {
+      sumx += x[i];
+      sumx2 += x[i] * x[i];
+      sumy += y[i];
+      sumy2 += y[i] * y[i];
+    }
+    float meanx = sumx / n;
+    float varx =  sumx2 / n - meanx * meanx; // E(x^2) - E(x)^2
+    float meany = sumy / n;
+    float vary =  sumy2 / n - meany * meany;
+    float correlation = 0;
+    for (int i = 0; i < n; ++i) {
+      correlation += (x[i] - meanx) * (y[i] - meany) / sqrt(varx * vary);
+    }
+    return correlation;
+  }
+
+  void ApplyHomography(const cv::Matx33d &H,
+                       float x1, float y1,
+                       float *x2, float *y2) {
+    float w = H(2, 0) * x1 + H(2, 1) * y1 + H(2, 2);
+    *x2 = (H(0, 0) * x1 + H(0, 1) * y1 + H(0, 2)) / w;
+    *y2 = (H(1, 0) * x1 + H(1, 1) * y1 + H(1, 2)) / w;
   }
 
   cv::Matx33d PlaneInducedHomography(const cv::Matx33d K1,
