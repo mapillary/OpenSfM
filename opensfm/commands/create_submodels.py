450de23081c8bde6dae09b444bb61826953c22e4
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2017-04-11 07:48:31 +0000

    Create submodel folder structure based on clusters.
    Use clusters with neighbors to create image list files.
    Copy config to make it editable for each submodel.
    Create symlinks to metadata folders.

--- opensfm/commands/create_submodels.py
+++ opensfm/commands/create_submodels.py
@@ -2,8 +2,10 @@ import csv
 import cv2
 import logging
 import numpy as np
+import os
 import os.path
 import scipy.spatial as spatial
+from shutil import copyfile
 
 from opensfm import dataset
 from opensfm import geo
@@ -27,6 +29,7 @@ class Command:
         self._create_image_list(data, meta_data)
         self._cluster_images(meta_data, args.cluster_size)
         self._create_clusters_with_neighbors(meta_data)
+        self._create_sub_model_structure(meta_data)
 
     def _create_image_list(self, data, meta_data):
         if not meta_data.image_list_exists():
@@ -92,6 +95,11 @@ class Command:
 
         meta_data.save_clusters_with_neighbors(clusters)
 
+    def _create_sub_model_structure(self, meta_data):
+        clusters = meta_data.load_clusters_with_neighbors()
+
+        meta_data.create_submodels(clusters)
+
 
 class MetaDataSet():
     def __init__(self, data_path):
@@ -102,7 +110,7 @@ class MetaDataSet():
         """
         self.data_path = data_path
 
-        self.__submodels_folder_name = 'submodels'
+        self.__submodels_dir_name = 'submodels'
         self.__image_list_file_name = 'image_list_with_gps.csv'
         self.__clusters_file_name = 'clusters.npz'
         self.__clusters_with_neighbors_file_name = 'clusters_with_neighbors.npz'
@@ -110,7 +118,7 @@ class MetaDataSet():
         io.mkdir_p(self.__submodels_path())
 
     def __submodels_path(self):
-        return os.path.join(self.data_path, self.__submodels_folder_name)
+        return os.path.join(self.data_path, self.__submodels_dir_name)
 
     def __image_list_path(self):
         return os.path.join(self.__submodels_path(), self.__image_list_file_name)
@@ -123,6 +131,15 @@ class MetaDataSet():
 
     def __create_csv_writer(self, csvfile):
         return csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    def _create_symlink(self, base_path, dir_name):
+        images_link_path = os.path.join(base_path, dir_name)
+        if os.path.exists(images_link_path):
+            os.unlink(images_link_path)
+
+        os.symlink(
+            os.path.relpath(os.path.join(self.data_path, dir_name), base_path),
+            os.path.join(images_link_path))
 
     def image_list_exists(self):
         return os.path.isfile(self.__image_list_path())
@@ -167,3 +184,26 @@ class MetaDataSet():
     def load_clusters_with_neighbors(self):
         c = np.load(self.__clusters_with_neighbors_path())
         return c['clusters']
+
+    def create_submodels(self, clusters):
+        for i, cluster in enumerate(clusters):
+            # create sub model dir
+            submodel_path = os.path.join(self.__submodels_path(), "submodel" + str(i))
+            io.mkdir_p(submodel_path)
+
+            # create image list file
+            image_list_path = os.path.join(submodel_path, "image_list.txt")
+            with open(image_list_path, 'w') as txtfile:
+                for image in cluster:
+                    txtfile.write(image + "\n")
+
+            # copy config.yaml if exists
+            config_file_path = os.path.join(self.data_path, 'config.yaml')
+            if os.path.exists(config_file_path):
+                copyfile(config_file_path, os.path.join(submodel_path, 'config.yaml'))
+
+            # create symlinks to metadata dirs
+            self._create_symlink(submodel_path, "images")
+            self._create_symlink(submodel_path, "exif")
+            self._create_symlink(submodel_path, "features")
+            self._create_symlink(submodel_path, "matches")
