43236bea9ff015391481f0cec4b4c35065c7a492
Pau Gargallo <pau.gargallo@gmail.com>
2017-05-31 17:30:52 +0200

    Find candidate matching pairs efficiently

--- opensfm/commands/match_features.py
+++ opensfm/commands/match_features.py
@@ -1,8 +1,9 @@
 import logging
-from multiprocessing import Pool
 import time
+from multiprocessing import Pool
 
 import numpy as np
+import scipy.spatial as spatial
 
 from opensfm import dataset
 from opensfm import geo
@@ -72,78 +73,100 @@ def load_preemptive_features(data):
 
 
 def has_gps_info(exif):
-    return (exif
-            and 'gps' in exif
-            and 'latitude' in exif['gps']
-            and 'longitude' in exif['gps'])
+    return (exif and
+            'gps' in exif and
+            'latitude' in exif['gps'] and
+            'longitude' in exif['gps'])
 
 
-def distance_from_exif(exif1, exif2):
-    """Compute distance between images based on exif metadata.
+def match_candidates_by_distance(images, exifs, reference, max_neighbors, max_distance):
+    """Find candidate matching pairs by GPS distance."""
+    if max_neighbors <= 0 and max_distance <= 0:
+        return set()
+    max_neighbors = max_neighbors or 99999999
+    max_distance = max_distance or 99999999.
+    k = min(len(images), max_neighbors + 1)
 
-    >>> exif1 = {'gps': {'latitude': 50.0663888889, 'longitude': 5.714722222}}
-    >>> exif2 = {'gps': {'latitude': 58.6438888889, 'longitude': 3.070000000}}
-    >>> d = distance_from_exif(exif1, exif2)
-    >>> abs(d - 968998) < 1
-    True
-    """
-    if has_gps_info(exif1) and has_gps_info(exif2):
-        gps1 = exif1['gps']
-        gps2 = exif2['gps']
-        latlon1 = gps1['latitude'], gps1['longitude']
-        latlon2 = gps2['latitude'], gps2['longitude']
-        return geo.gps_distance(latlon1, latlon2)
-    else:
-        return 0
+    points = np.zeros((len(images), 3))
+    for i, image in enumerate(images):
+        gps = exifs[image]['gps']
+        points[i] = geo.topocentric_from_lla(
+            gps['latitude'], gps['longitude'], gps['altitude'],
+            reference['latitude'], reference['longitude'], reference['altitude'])
+
+    tree = spatial.cKDTree(points)
+
+    pairs = set()
+    for i, image in enumerate(images):
+        distances, neighbors = tree.query(
+            points[i], k=k, distance_upper_bound=max_distance)
+        for j in neighbors:
+            if i != j and j < len(images):
+                pairs.add(tuple(sorted((images[i], images[j]))))
+    return pairs
 
 
-def timediff_from_exif(exif1, exif2):
-    return np.fabs(exif1['capture_time'] - exif2['capture_time'])
+def match_candidates_by_time(images, exifs, max_neighbors):
+    """Find candidate matching pairs by time difference."""
+    if max_neighbors <= 0:
+        return set()
+    k = min(len(images), max_neighbors + 1)
+
+    times = np.zeros((len(images), 1))
+    for i, image in enumerate(images):
+        times[i] = exifs[image]['capture_time']
+
+    tree = spatial.cKDTree(times)
+
+    pairs = set()
+    for i, image in enumerate(images):
+        distances, neighbors = tree.query(times[i], k=k)
+        for j in neighbors:
+            if i != j and j < len(images):
+                pairs.add(tuple(sorted((images[i], images[j]))))
+    return pairs
+
+
+def match_candidates_by_order(images, exifs, max_neighbors):
+    """Find candidate matching pairs by sequence order."""
+    if max_neighbors <= 0:
+        return set()
+    n = (max_neighbors + 1) / 2
+
+    pairs = set()
+    for i, image in enumerate(images):
+        a = max(0, i - n)
+        b = min(len(images), i + n)
+        for j in range(a, b):
+            if i != j:
+                pairs.add(tuple(sorted((images[i], images[j]))))
+    return pairs
 
 
 def match_candidates_from_metadata(images, exifs, data):
-    '''
-    Compute candidate matching pairs based on GPS and capture time
-    '''
+    """Compute candidate matching pairs"""
     max_distance = data.config['matching_gps_distance']
-    max_neighbors = data.config['matching_gps_neighbors']
-    max_time_neighbors = data.config['matching_time_neighbors']
-    max_order_neighbors = data.config['matching_order_neighbors']
+    gps_neighbors = data.config['matching_gps_neighbors']
+    time_neighbors = data.config['matching_time_neighbors']
+    order_neighbors = data.config['matching_order_neighbors']
 
-    if not all(map(has_gps_info, exifs.values())) and max_neighbors != 0:
+    if not data.reference_lla_exists():
+        data.invent_reference_lla()
+    reference = data.load_reference_lla()
+
+    if not all(map(has_gps_info, exifs.values())) and gps_neighbors != 0:
         logger.warn("Not all images have GPS info. "
                     "Disabling matching_gps_neighbors.")
-        max_neighbors = 0
+        gps_neighbors = 0
+        max_distance = 0
 
-    pairs = set()
     images.sort()
-    for index1, im1 in enumerate(images):
-        distances = []
-        timediffs = []
-        indexdiffs = []
-        for index2, im2 in enumerate(images):
-            if im1 != im2:
-                dx = distance_from_exif(exifs[im1], exifs[im2])
-                dt = timediff_from_exif(exifs[im1], exifs[im2])
-                di = abs(index1 - index2)
-                if dx <= max_distance:
-                    distances.append((dx, im2))
-                    timediffs.append((dt, im2))
-                    indexdiffs.append((di, im2))
-        distances.sort()
-        timediffs.sort()
-        indexdiffs.sort()
 
-        if max_neighbors or max_time_neighbors or max_order_neighbors:
-            distances = distances[:max_neighbors]
-            timediffs = timediffs[:max_time_neighbors]
-            indexdiffs = indexdiffs[:max_order_neighbors]
-
-        for d, im2 in distances + timediffs + indexdiffs:
-            if im1 < im2:
-                pairs.add((im1, im2))
-            else:
-                pairs.add((im2, im1))
+    d = match_candidates_by_distance(images, exifs, reference,
+                                     gps_neighbors, max_distance)
+    t = match_candidates_by_time(images, exifs, time_neighbors)
+    o = match_candidates_by_order(images, exifs, order_neighbors)
+    pairs = d | t | o
 
     res = {im: [] for im in images}
     for im1, im2 in pairs:
@@ -157,9 +180,7 @@ def match_arguments(pairs, ctx):
 
 
 def match(args):
-    '''
-    Compute all matches for a single image
-    '''
+    """Compute all matches for a single image"""
     im1, candidates, i, n, ctx = args
     logger.info('Matching {}  -  {} / {}'.format(im1, i + 1, n))
 
