db5d40615d6df6926eeb69ba723d44f0923cb164
Pau Gargallo <pau.gargallo@gmail.com>
2017-12-10 23:40:38 +0100

    feat(large): add option to provide groups for creating submodels

--- opensfm/commands/create_submodels.py
+++ opensfm/commands/create_submodels.py
@@ -1,5 +1,6 @@
 import logging
 import numpy as np
+from collections import defaultdict
 
 from opensfm import dataset
 from opensfm.large import metadataset
@@ -14,6 +15,10 @@ class Command:
 
     def add_arguments(self, parser):
         parser.add_argument('dataset', help='dataset to process')
+        parser.add_argument(
+            '--groups',
+            help='text file with group definitions. '
+                 'Each line should contain an image and its group.')
 
     def run(self, args):
         data = dataset.DataSet(args.dataset)
@@ -23,7 +28,10 @@ class Command:
         data.invent_reference_lla()
 
         self._create_image_list(data, meta_data)
-        self._cluster_images(meta_data, data.config['submodel_size'])
+        if args.groups:
+            self._read_image_clusters(meta_data, args.groups)
+        else:
+            self._cluster_images(meta_data, data.config['submodel_size'])
         self._add_cluster_neighbors(meta_data, data.config['submodel_overlap'])
 
         meta_data.create_submodels(
@@ -46,6 +54,39 @@ class Command:
             ills.append((image, lat, lon))
 
         meta_data.create_image_list(ills)
+
+    def _read_image_clusters(self, meta_data, groups_file):
+        image_cluster = {}
+        cluster_images = defaultdict(list)
+        with open(groups_file) as fin:
+            for line in fin:
+                image, cluster = line.split()
+                image_cluster[image] = cluster
+                cluster_images[cluster].append(image)
+        K = len(cluster_images)
+        cluster_index = dict(zip(sorted(cluster_images.keys()), range(K)))
+
+        images = []
+        positions = []
+        labels = []
+        centers = np.zeros((K, 2))
+        centers_count = np.zeros((K, 1))
+        for image, lat, lon in meta_data.images_with_gps():
+            images.append(image)
+            positions.append([lat, lon])
+            cluster = image_cluster[image]
+            label = cluster_index[cluster]
+            labels.append(label)
+            centers[label, 0] += lat
+            centers[label, 1] += lon
+            centers_count[label] += 1
+
+        images = np.array(images).reshape((len(images), 1))
+        positions = np.array(positions, np.float32)
+        labels = np.array(labels)
+        centers /= centers_count
+
+        meta_data.save_clusters(images, positions, labels, centers)
 
     def _cluster_images(self, meta_data, cluster_size):
         images = []
