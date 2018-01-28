2c8136558f91117edcffa967915352a1c63d38a4
Pau Gargallo <pau.gargallo@gmail.com>
2018-01-28 01:27:23 +0100

    fix: lower case in a python 2/3 compatible

--- opensfm/sensors.py
+++ opensfm/sensors.py
@@ -6,6 +6,6 @@ with open(context.SENSOR, 'rb') as f:
     sensor_data = json.loads(f.read())
 
 # Convert model types to lower cases for easier query
-keys = map(str.lower, sensor_data.keys())
+keys = [k.lower() for k in sensor_data.keys()]
 values = sensor_data.values()
 sensor_data = dict(zip(keys, values))
