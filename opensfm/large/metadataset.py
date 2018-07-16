afa546be98e790ce47db7b4e005eaa2e812e8e03
Pau Gargallo <pau.gargallo@gmail.com>
2018-07-16 16:50:24 +0200

    fix: link path relative to destination folder

--- opensfm/large/metadataset.py
+++ opensfm/large/metadataset.py
@@ -149,8 +149,9 @@ class MetaDataSet():
                 for image in cluster:
                     src = data.image_files[image]
                     dst = os.path.join(submodel_images_path, image)
+                    src_relpath = os.path.relpath(src, submodel_images_path)
                     if not os.path.isfile(dst):
-                        os.symlink(src, dst)
+                        os.symlink(src_relpath, dst)
                     dst_relpath = os.path.relpath(dst, submodel_path)
                     txtfile.write(dst_relpath + "\n")
 
