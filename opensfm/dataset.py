b8da8d4185198d8c3a1eb1981d05bc19a80ae9b3
Piero Toffanin <pt@masseranolabs.com>
2018-08-10 12:05:47 -0400

    Revert "Simplified export_visualsfm command, allow compute_depthmaps to use PNG"
    This reverts commit e37dba9d29ddb040dee73bde8a0f3de8f02bc588.

--- opensfm/dataset.py
+++ opensfm/dataset.py
@@ -82,11 +82,7 @@ class DataSet:
 
     def load_undistorted_image(self, image):
         """Load undistorted image pixels as a numpy array."""
-        for extension in ['jpg', 'png']:
-            if os.path.exists(self._undistorted_image_file(image, extension)):
-                return io.imread(self._undistorted_image_file(image, extension))
-        
-        raise OSError("Cannot read image {}. File does not exist.".format(image))
+        return io.imread(self._undistorted_image_file(image))
 
     def save_undistorted_image(self, image, array, image_format = 'jpg'):
         io.mkdir_p(self._undistorted_image_path())
