fix: fix rotation error metric and right-hand sidenes of cameras

opensfm/synthetic_data/synthetic_generator.py
@@ -34,9 +34,9 @@ def generate_samples_and_local_frame(samples, shape):
     for i in samples:
         point = shape(i)
         points += [point]
-        ez = derivative(shape, i)
-        ex = np.array([-ez[1], ez[0]])
-        tangents += [np.array([ex, ez])]
+        ex = derivative(shape, i)
+        ez = np.array([ex[1], -ex[0]])
+        tangents += [np.array([ez, ex])]
     return np.array(points), np.array(tangents)
 
 
@@ -84,7 +84,7 @@ def generate_cameras(samples, shape, height):
     positions, rotations = generate_samples_and_local_frame(samples, shape)
     positions = np.insert(positions, 2, values=height, axis=1)
     rotations = np.insert(rotations, 2, values=0, axis=2)
-    rotations = np.insert(rotations, 1, values=np.array([0, 0, 1]), axis=1)
+    rotations = np.insert(rotations, 1, values=np.array([0, 0, -1]), axis=1)
     return positions, rotations
 
 

opensfm/synthetic_data/synthetic_scene.py
@@ -121,6 +121,8 @@ def position_errors(reference, candidate):
     for s in common_shots:
         pose1 = reference.shots[s].pose.get_origin()
         pose2 = candidate.shots[s].pose.get_origin()
+        pose1[2] = pose2[2]
+        error = np.linalg.norm(pose1-pose2)
         errors.append(np.linalg.norm(pose1-pose2))
     return np.array(errors)
 
@@ -132,7 +134,7 @@ def rotation_errors(reference, candidate):
     for s in common_shots:
         pose1 = reference.shots[s].pose.get_rotation_matrix()
         pose2 = candidate.shots[s].pose.get_rotation_matrix()
-        difference = np.transpose(pose1)*pose2
+        difference = np.transpose(pose1).dot(pose2)
         rodrigues = cv2.Rodrigues(difference)[0].ravel()
         angle = np.linalg.norm(rodrigues)
         errors.append(angle)