56186181d3e4be904f91854c4cc4dd1f837f6061
Pau Gargallo <pau.gargallo@gmail.com>
2019-01-30 16:59:14 +0100

    chore: add logging

--- opensfm/commands/detect_features.py
+++ opensfm/commands/detect_features.py
@@ -51,42 +51,48 @@ class Command:
 
 
 def detect(args):
+    image, data = args
+
     log.setup()
 
-    image, data = args
+    if data.feature_index_exists(image):
+        logger.info('Skip recomputing {} features for image {}'.format(
+            data.feature_type().upper(), image))
+        return
+
     logger.info('Extracting {} features for image {}'.format(
         data.feature_type().upper(), image))
 
-    if not data.feature_index_exists(image):
-        start = timer()
-        mask = data.load_combined_mask(image)
-        if mask is not None:
-            logger.info('Found mask to apply for image {}'.format(image))
-        preemptive_max = data.config['preemptive_max']
-        p_unsorted, f_unsorted, c_unsorted = features.extract_features(
-            data.load_image(image), data.config, mask)
-        if len(p_unsorted) == 0:
-            return
+    start = timer()
+    mask = data.load_combined_mask(image)
+    if mask is not None:
+        logger.info('Found mask to apply for image {}'.format(image))
+    preemptive_max = data.config['preemptive_max']
+    p_unsorted, f_unsorted, c_unsorted = features.extract_features(
+        data.load_image(image), data.config, mask)
 
-        size = p_unsorted[:, 2]
-        order = np.argsort(size)
-        p_sorted = p_unsorted[order, :]
-        f_sorted = f_unsorted[order, :]
-        c_sorted = c_unsorted[order, :]
-        p_pre = p_sorted[-preemptive_max:]
-        f_pre = f_sorted[-preemptive_max:]
-        data.save_features(image, p_sorted, f_sorted, c_sorted)
-        data.save_preemptive_features(image, p_pre, f_pre)
+    if len(p_unsorted) == 0:
+        logger.warning('No features found in image {}'.format(image))
+        return
 
-        if data.config['matcher_type'] == 'FLANN':
-            index = features.build_flann_index(f_sorted, data.config)
-            data.save_feature_index(image, index)
+    size = p_unsorted[:, 2]
+    order = np.argsort(size)
+    p_sorted = p_unsorted[order, :]
+    f_sorted = f_unsorted[order, :]
+    c_sorted = c_unsorted[order, :]
+    p_pre = p_sorted[-preemptive_max:]
+    f_pre = f_sorted[-preemptive_max:]
+    data.save_features(image, p_sorted, f_sorted, c_sorted)
+    data.save_preemptive_features(image, p_pre, f_pre)
 
-        end = timer()
-        report = {
-            "image": image,
-            "num_features": len(p_sorted),
-            "wall_time": end - start,
-        }
-        data.save_report(io.json_dumps(report),
-                         'features/{}.json'.format(image))
+    if data.config['matcher_type'] == 'FLANN':
+        index = features.build_flann_index(f_sorted, data.config)
+        data.save_feature_index(image, index)
+
+    end = timer()
+    report = {
+        "image": image,
+        "num_features": len(p_sorted),
+        "wall_time": end - start,
+    }
+    data.save_report(io.json_dumps(report), 'features/{}.json'.format(image))
