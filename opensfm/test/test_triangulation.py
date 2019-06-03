71c7f5c3635e0bd8ad78187528b6733fbe63276f
Yann Noutary <yann@mapillary.com>
2019-06-03 17:30:02 +0200

    fix: make test happy and testing

--- opensfm/test/test_triangulation.py
+++ opensfm/test/test_triangulation.py
@@ -11,8 +11,8 @@ def test_track_triangulator_equirectangular():
     graph.add_node('im1', bipartite=0)
     graph.add_node('im2', bipartite=0)
     graph.add_node('1', bipartite=1)
-    graph.add_edge('im1', '1', feature=(0, 0))
-    graph.add_edge('im2', '1', feature=(-0.1, 0))
+    graph.add_edge('im1', '1', feature=(0, 0), feature_scale=1.0, feature_id=0, feature_color=0)
+    graph.add_edge('im2', '1', feature=(-0.1, 0), feature_scale=1.0, feature_id=1, feature_color=0)
 
     reconstruction = io.reconstruction_from_json({
         "cameras": {
@@ -40,12 +40,14 @@ def test_track_triangulator_equirectangular():
         },
     })
 
-    triangulator = opensfm.reconstruction.TrackTriangulator(graph,
+    graph_inliers = nx.Graph()
+    triangulator = opensfm.reconstruction.TrackTriangulator(graph, graph_inliers,
                                                             reconstruction)
     triangulator.triangulate('1', 0.01, 2.0)
     assert '1' in reconstruction.points
     p = reconstruction.points['1'].coordinates
     assert np.allclose(p, [0, 0, 1.3763819204711])
+    assert len(graph_inliers.edges()) == 2
 
 
 def unit_vector(x):
