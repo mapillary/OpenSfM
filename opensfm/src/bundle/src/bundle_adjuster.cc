22a7eadbcb447b75cabf658b10d8e5228ad71c32
Yann Noutary <yann@mapillary.com>
2019-11-15 16:05:54 +0100

    feat: soft constraint transition using log-barrier penalty

--- opensfm/src/bundle/src/bundle_adjuster.cc
+++ opensfm/src/bundle/src/bundle_adjuster.cc
@@ -480,6 +480,25 @@ struct BAStdDeviationConstraint {
   }
 };
 
+struct BAParameterBarrier {
+  BAParameterBarrier(double lower_bound, double upper_bound, int index)
+      : lower_bound_(lower_bound), upper_bound_(upper_bound), index_(index) {}
+
+  template <typename T>
+  bool operator()(const T* const parameters, T* residuals) const {
+    T eps = T(1e-10);
+    T value = parameters[index_];
+    T zero = 2.0*ceres::log((T(upper_bound_)-T(lower_bound_))*0.5);
+    T penalty = ceres::log(value-T(lower_bound_)+eps)+ceres::log(T(upper_bound_)-value+eps);
+    residuals[0] = penalty + zero;
+    return true;
+  }
+
+  double lower_bound_;
+  double upper_bound_;
+  int index_;
+};
+
 void BundleAdjuster::Run() {
   ceres::Problem problem;
 
@@ -494,40 +513,51 @@ void BundleAdjuster::Run() {
   }
 
   for (auto &i : cameras_) {
-    if (i.second->constant) {
-      switch (i.second->type()) {
-        case BA_PERSPECTIVE_CAMERA: {
-          BAPerspectiveCamera &c =
-              static_cast<BAPerspectiveCamera &>(*i.second);
-          problem.AddParameterBlock(c.parameters, BA_CAMERA_NUM_PARAMS);
+    switch (i.second->type()) {
+      case BA_PERSPECTIVE_CAMERA: {
+        BAPerspectiveCamera &c =
+            static_cast<BAPerspectiveCamera &>(*i.second);
+        problem.AddParameterBlock(c.parameters, BA_CAMERA_NUM_PARAMS);
+        if (i.second->constant) {
           problem.SetParameterBlockConstant(c.parameters);
-          break;
         }
-        case BA_BROWN_PERSPECTIVE_CAMERA: {
-          BABrownPerspectiveCamera &c =
-              static_cast<BABrownPerspectiveCamera &>(*i.second);
-          problem.AddParameterBlock(c.parameters, BA_BROWN_CAMERA_NUM_PARAMS);
-          problem.SetParameterBlockConstant(c.parameters);
-          break;
-        }
-        case BA_FISHEYE_CAMERA: {
-          BAFisheyeCamera &c = static_cast<BAFisheyeCamera &>(*i.second);
-          problem.AddParameterBlock(c.parameters, BA_CAMERA_NUM_PARAMS);
-          problem.SetParameterBlockConstant(c.parameters);
-          break;
-        }
-        case BA_DUAL_CAMERA: {
-          BADualCamera &c = static_cast<BADualCamera &>(*i.second);
-          problem.AddParameterBlock(c.parameters, BA_CAMERA_NUM_PARAMS);
-          problem.SetParameterBlockConstant(c.parameters);
-          break;
-        }
-        case BA_EQUIRECTANGULAR_CAMERA:
-          // No parameters for now
-          break;
+        break;
       }
+      case BA_BROWN_PERSPECTIVE_CAMERA: {
+        BABrownPerspectiveCamera &c =
+            static_cast<BABrownPerspectiveCamera &>(*i.second);
+        problem.AddParameterBlock(c.parameters, BA_BROWN_CAMERA_NUM_PARAMS);
+        if (i.second->constant) {
+          problem.SetParameterBlockConstant(c.parameters);
+        }
+        break;
+      }
+      case BA_FISHEYE_CAMERA: {
+        BAFisheyeCamera &c = static_cast<BAFisheyeCamera &>(*i.second);
+        problem.AddParameterBlock(c.parameters, BA_CAMERA_NUM_PARAMS);
+        if (i.second->constant) {
+          problem.SetParameterBlockConstant(c.parameters);
+        }
+        break;
+      }
+      case BA_DUAL_CAMERA: {
+        BADualCamera &c = static_cast<BADualCamera &>(*i.second);
+        problem.AddParameterBlock(c.parameters, BA_CAMERA_NUM_PARAMS);
+        ceres::CostFunction* transition_barrier =
+            new ceres::AutoDiffCostFunction<BAParameterBarrier, 1, BA_CAMERA_NUM_PARAMS>(
+                new BAParameterBarrier(0.0, 1.0, BA_CAMERA_TRANSITION));
+        problem.AddResidualBlock(transition_barrier, NULL, c.parameters);
+        if (i.second->constant) {
+          problem.SetParameterBlockConstant(c.parameters);
+        }
+        break;
+      }
+      case BA_EQUIRECTANGULAR_CAMERA:
+        // No parameters for now
+        break;
     }
   }
+  
   for (auto &i : reconstructions_) {
     for (auto &s : i.second.scales) {
       if (i.second.constant) {
