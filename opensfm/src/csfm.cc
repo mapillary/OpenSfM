9ef6c68820490ff8b4f63198aaa1bc17b48fc9a0
Yann Noutary <yann@mapillary.com>
2019-12-23 17:04:43 +0100

    fix: fixed tests

--- opensfm/src/csfm.cc
+++ opensfm/src/csfm.cc
@@ -225,7 +225,7 @@ PYBIND11_MODULE(csfm, m) {
   py::class_<BARelativeMotion>(m, "BARelativeMotion")
     .def(py::init<const std::string &, const std::string &, 
 	        const std::string &, const std::string &, 
-				  const Eigen::Vector3d &, const Eigen::Vector3d &,
+          const Eigen::Vector3d &, const Eigen::Vector3d &,
           double>())
     .def_readwrite("reconstruction_i", &BARelativeMotion::reconstruction_id_i)
     .def_readwrite("shot_i", &BARelativeMotion::shot_id_i)
@@ -239,7 +239,7 @@ PYBIND11_MODULE(csfm, m) {
   py::class_<BARelativeSimilarity>(m, "BARelativeSimilarity")
     .def(py::init<const std::string &, const std::string &, 
 	        const std::string &, const std::string &, 
-				  const Eigen::Vector3d &, const Eigen::Vector3d &,
+	        const Eigen::Vector3d &, const Eigen::Vector3d &,
           double, double>())
     .def_readwrite("scale", &BARelativeSimilarity::scale)
     .def("set_scale_matrix", &BARelativeSimilarity::SetScaleMatrix)
