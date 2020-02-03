2a41d5d09861c1d05ea616dcf292f59a37a0004c
Yann Noutary <yann@mapillary.com>
2020-02-03 14:58:19 +0100

    feat: add a small scene example in straight line

--- opensfm/synthetic_data/synthetic_examples.py
+++ opensfm/synthetic_data/synthetic_examples.py
@@ -24,3 +24,23 @@ def synthetic_ellipse_scene():
                               position_perturbation,
                               rotation_perturbation)
     return scene
+
+
+def synthetic_small_line_scene():
+    scene_length = 15
+    points_count = 5000
+    generator = get_scene_generator('line', scene_length)
+    scene = SyntheticScene(generator)
+    scene.add_street(points_count, 7, 7).perturb_floor([0, 0, 0.1]).\
+        perturb_walls([0.2, 0.2, 0.01])
+
+    camera_height = 1.5
+    camera_interval = 3
+    position_perturbation = [0.2, 0.2, 0.01]
+    rotation_perturbation = 0.3
+    camera = get_camera('perspective', '1', 0.9, -0.1, 0.01)
+    scene.add_camera_sequence(camera, 0, scene_length,
+                              camera_height, camera_interval,
+                              position_perturbation,
+                              rotation_perturbation)
+    return scene
