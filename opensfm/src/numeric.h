54243e15bd2efcf30679d821f193a2e4f9f7869f
Yann Noutary <yann@mapillary.com>
2020-02-07 16:01:47 +0100

    fix: return ASAP for underconstrained systems

--- opensfm/src/numeric.h
+++ opensfm/src/numeric.h
@@ -4,19 +4,23 @@
 
 template<class MAT, class VEC>
 bool SolveAX0(const MAT& A, VEC* solution){
+  // Don't solve under-constrained systems
+  if(A.rows() < A.cols()){
+    return false;
+  }
+
   Eigen::JacobiSVD<MAT> svd(A, Eigen::ComputeFullV);
   const int data_size = A.cols();
   *solution = svd.matrixV().col(data_size-1);
 
   // Check ratio of smallest eigenvalues for single nullspace
-  const double minimum_ratio = 1e3;
+  const double minimum_ratio = 4;
   const auto values = svd.singularValues();
   const double ratio = values(data_size-2)/values(data_size-1);
 
-  // Enough constraint and some nullspace will make a solution
-  const bool constrained = A.rows() >= A.cols();
+  // Some nullspace will make a solution
   const bool some_nullspace = ratio > minimum_ratio;
-  if (constrained && some_nullspace)
+  if (some_nullspace)
     return true;
   else
     return false;
