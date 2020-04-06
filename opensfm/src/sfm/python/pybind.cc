c300b862f5f921fa5d246e5be07b986428d206a9
Yann Noutary <yann@mapillary.com>
2020-04-06 16:24:55 +0200

    feat: expose keypoint

--- opensfm/src/sfm/python/pybind.cc
+++ opensfm/src/sfm/python/pybind.cc
@@ -4,12 +4,23 @@
 #include <glog/logging.h>
 
 #include <sfm/tracks_manager.h>
+#include <sfm/keypoint.h>
 #include <foundation/types.h>
 
 
 PYBIND11_MODULE(pysfm, m) {
+
+  py::class_<Keypoint>(m, "Keypoint")
+    .def(py::init())
+    .def_readwrite("point", &Keypoint::point)
+    .def_readwrite("scale", &Keypoint::scale)
+    .def_readwrite("id", &Keypoint::id)
+    .def_readwrite("color", &Keypoint::color)
+  ;
+
   py::class_<TracksManager>(m, "TracksManager")
     .def(py::init())
+    .def("add_track", &TracksManager::AddTrack)
     .def("get_shot_ids", &TracksManager::GetShotIds)
     .def("get_track_ids", &TracksManager::GetTrackIds)
     .def("get_observation", &TracksManager::GetObservation)
