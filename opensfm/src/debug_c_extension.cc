a37c183237d17549b20557e573ccb901e2195858
Pau Gargallo <pau.gargallo@gmail.com>
2020-04-09 11:30:13 +0200

    feat: use pybind11 embed for debug c extension

--- opensfm/src/debug_c_extension.cc
+++ opensfm/src/debug_c_extension.cc
@@ -2,20 +2,27 @@
 //
 // How to use it:
 //
-//   1. Write a function in python that calls the extension you want to debug.
-//      Make sure it can be called without arguments.
+//   1. Write a module in python that calls the extension you want to debug.
+//      Make sure the extension code is called at import time.
 //
 //   2. Build opensfm/cmake_build in Debug mode.
 //
 //   3. Tell the debugger to run
 //
-//        ./debug_c_extension path module function
+//        ./debug_c_extension module
 //
-//      where path is the path to the module that contains the function
-//      that you wrote.
+//      where module is the name of the module that you wrote.
 
-#include "third_party/PythonExtensionPatterns/py_import_call_execute.h"
+#include <iostream>
+#include <pybind11/embed.h>
+namespace py = pybind11;
 
-int main(int argc, const char *argv[]) {
-    import_call_execute(argc, argv);
+int main(int argc, char *argv[]) {
+    py::scoped_interpreter guard{};
+
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " module" << std::endl;
+    } else {
+        py::module sys = py::module::import(argv[1]);
+    }
 }
