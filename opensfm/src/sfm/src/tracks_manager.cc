61bc22cc3b1b0a825504fa7a6e465ca1ebadc10f
Pau Gargallo <pau.gargallo@gmail.com>
2020-05-03 21:55:05 +0200

    chore: use emplace and simplify conditionals

--- opensfm/src/sfm/src/tracks_manager.cc
+++ opensfm/src/sfm/src/tracks_manager.cc
@@ -212,10 +212,9 @@ TracksManager::GetAllCommonObservations(const ShotId& shot1,
   std::vector<KeyPointTuple> tuples;
   for (const auto& p : find_shot1->second) {
     const auto find = find_shot2->second.find(p.first);
-    if(find == find_shot2->second.end()){
-      continue;
+    if (find != find_shot2->second.end()) {
+      tuples.emplace_back(p.first, p.second, find->second);
     }
-    tuples.push_back(std::make_tuple(p.first, p.second, find->second));
   }
   return tuples;
 }
@@ -227,55 +226,41 @@ TracksManager::GetAllPairsConnectivity(
   std::unordered_map<ShotPair, int, HashPair> common_per_pair;
 
   std::vector<TrackId> tracks_to_use;
-  if(tracks.empty()){
-    for (const auto& track : shot_per_tracks_){
+  if (tracks.empty()) {
+    for (const auto& track : shot_per_tracks_) {
       tracks_to_use.push_back(track.first);
     }
-  }
-  else{
+  } else {
     tracks_to_use = tracks;
   }
 
   std::unordered_set<ShotId> shots_to_use;
-  if(shots.empty()){
-    for (const auto& shot : tracks_per_shot_){
+  if (shots.empty()) {
+    for (const auto& shot : tracks_per_shot_) {
       shots_to_use.insert(shot.first);
     }
-  }
-  else{
-    for (const auto& shot : shots){
+  } else {
+    for (const auto& shot : shots) {
       shots_to_use.insert(shot);
     }
   }
 
-
   for (const auto& track_id : tracks_to_use) {
     const auto find_track = shot_per_tracks_.find(track_id);
-    if(find_track == shot_per_tracks_.end()){
+    if (find_track == shot_per_tracks_.end()) {
       continue;
     }
     const auto& track = find_track->second;
-    for (std::unordered_map<ShotId, Observation>::const_iterator it1 =
-             track.begin();
-         it1 != track.end(); ++it1) {
-      const auto& shotID1 = it1->first;
-      if(shots_to_use.find(shotID1) == shots_to_use.end()){
-        continue;
-      }
-      for (std::unordered_map<ShotId, Observation>::const_iterator it2 =
-               track.begin();
-           it2 != track.end(); ++it2) {
-        const auto& shotID2 = it2->first;
-        if(shots_to_use.find(shotID2) == shots_to_use.end()){
-          continue;
+    for (const auto& it1 : track) {
+      const auto& shot_id1 = it1.first;
+      if (shots_to_use.find(shot_id1) != shots_to_use.end()) {
+        for (const auto& it2 : track) {
+          const auto& shot_id2 = it2.first;
+          if (shot_id1 < shot_id2 &&
+              shots_to_use.find(shot_id2) != shots_to_use.end()) {
+            ++common_per_pair[std::make_pair(shot_id1, shot_id2)];
+          }
         }
-        if (shotID1 == shotID2) {
-          continue;
-        }
-        if (shotID1 > shotID2) {
-          continue;
-        }
-        ++common_per_pair[std::make_pair(shotID1, shotID2)];
       }
     }
   }
