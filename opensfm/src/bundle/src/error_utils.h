a27375bd5202e053146065caef51f235085dbf4d
Yann Noutary <yann@mapillary.com>
2020-05-18 14:36:12 +0200

    refactor: more refactor

--- opensfm/src/bundle/src/error_utils.h
+++ opensfm/src/bundle/src/error_utils.h
@@ -1,19 +1,21 @@
 #pragma once
 
 #include <initializer_list>
+
 #include "ceres/ceres.h"
 #include "ceres/rotation.h"
 
-// helper to entangle Eigen stuff and get base type
-template<class T>
+/* helper to entangle Eigen stuff and get base type */
+template <class T>
 using MatrixType = typename Eigen::MatrixBase<T>::Scalar;
 
-// multiply a set of N rotation R1*R2*...Rn-1=R
-// rotations are expected to be angle-axis
+/* multiply a set of N rotation R1*R2*...Rn-1=R rotations are expected to be
+ * angle-axis*/
 template <class T1, class... T>
-Eigen::Matrix<MatrixType<T1>, 3, 1> MultRotations(const T1& R1, T... R){
+Eigen::Matrix<MatrixType<T1>, 3, 1> MultRotations(const T1& R1, T... R) {
   // work-around to run over variadic
-  std::initializer_list<Eigen::Matrix<MatrixType<T1>, 3, 1> > rotations = {R...};
+  std::initializer_list<Eigen::Matrix<MatrixType<T1>, 3, 1> > rotations = {
+      R...};
 
   // hence why we split the variadic with a 1st argument
   Eigen::Array<MatrixType<T1>, 4, 1> qPrevious_Ri;
@@ -33,8 +35,8 @@ Eigen::Matrix<MatrixType<T1>, 3, 1> MultRotations(const T1& R1, T... R){
   return result;
 }
 
-// apply a rotation R to a vector x as R*x
-// rotations is expected to be angle-axis
+/* apply a rotation R to a vector x as R*x rotations is expected to be
+ * angle-axis */
 template <class T1, class T2>
 Eigen::Matrix<MatrixType<T1>, 3, 1> RotatePoint(const T1& R, const T2& x) {
   Eigen::Matrix<MatrixType<T1>, 3, 1> rotated;
@@ -42,11 +44,18 @@ Eigen::Matrix<MatrixType<T1>, 3, 1> RotatePoint(const T1& R, const T2& x) {
   return rotated;
 }
 
+/* bring a point x in the coordinate frame of a camera with rotation and camera
+ * center in world coordinates being respectively R and c such : x(camera) =
+ * R(t).(x(world) - c) */
+template <typename T1, typename T2, typename T3>
+void WorldToCamera(const T1& R, const T2& c, const T3& x) {
+  return RotatePoint(-R, (x - c));
+}
 
-// apply a similarity transform of scale s, rotation R and translation t
-// to some point x as s * R * x + t
+/* apply a similarity transform of scale s, rotation R and translation t to some
+ * point x as s * R * x + t */
 template <class T1, class T2, class T3, class T4>
-Eigen::Matrix<MatrixType<T2>, 3, 1> ApplySimilarity(const T1& s, const T2& R, 
+Eigen::Matrix<MatrixType<T2>, 3, 1> ApplySimilarity(const T1& s, const T2& R,
                                                     const T3& t, const T4& x) {
   return RotatePoint((-R).eval(), (s * x - t).eval());
 }
