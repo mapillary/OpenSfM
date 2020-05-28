70144c246c0d97ea7434b4103d1d5db3b6d7130d
Pau Gargallo <pau.gargallo@gmail.com>
2020-05-29 00:03:45 +0200

    fix: do not use inliers_graph

--- opensfm/test/test_robust.py
+++ opensfm/test/test_robust.py
@@ -211,9 +211,8 @@ def test_outliers_relative_pose_ransac(pairs_and_their_E):
         result = pyrobust.ransac_relative_pose(
             f1, f2, scale*(1.0+scale_eps_ratio), params, pyrobust.RansacType.RANSAC)
 
-        pose = copy.deepcopy(pose)
-        pose.translation /= np.linalg.norm(pose.translation)
         expected = pose.get_world_to_cam()[:3]
+        expected[:, 3] /= np.linalg.norm(expected[:, 3])
 
         tolerance = 0.1
         inliers_count = (1 - ratio_outliers) * len(points)
