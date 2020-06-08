64e2681690af9a26b4254c83d8a847d4393af180
Pau Gargallo <pau.gargallo@gmail.com>
2020-06-08 22:15:22 +0200

    chore: do not import *

--- opensfm/synthetic_data/synthetic_examples.py
+++ opensfm/synthetic_data/synthetic_examples.py
@@ -3,14 +3,14 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from opensfm.synthetic_data.synthetic_scene import *
+import opensfm.synthetic_data.synthetic_scene as ss
 
 
 def synthetic_circle_scene():
     scene_length = 60
     points_count = 5000
-    generator = get_scene_generator('circle', scene_length)
-    scene = SyntheticStreetScene(generator)
+    generator = ss.get_scene_generator('circle', scene_length)
+    scene = ss.SyntheticStreetScene(generator)
     scene.add_street(points_count, 7, 7).perturb_floor([0, 0, 0.1]).\
         perturb_walls([0.2, 0.2, 0.01])
 
@@ -18,7 +18,7 @@ def synthetic_circle_scene():
     camera_interval = 3
     position_perturbation = [0.2, 0.2, 0.01]
     rotation_perturbation = 0.2
-    camera = get_camera('perspective', '1', 0.9, -0.1, 0.01)
+    camera = ss.get_camera('perspective', '1', 0.9, -0.1, 0.01)
     scene.add_camera_sequence(camera, 0, scene_length,
                               camera_height, camera_interval,
                               position_perturbation,
@@ -27,14 +27,14 @@ def synthetic_circle_scene():
 
 
 def synthetic_cube_scene():
-    return SyntheticCubeScene(10, 1000, 0.001)
+    return ss.SyntheticCubeScene(10, 1000, 0.001)
 
 
 def synthetic_small_line_scene():
     scene_length = 15
     points_count = 5000
-    generator = get_scene_generator('line', scene_length)
-    scene = SyntheticStreetScene(generator)
+    generator = ss.get_scene_generator('line', scene_length)
+    scene = ss.SyntheticStreetScene(generator)
     scene.add_street(points_count, 7, 7).perturb_floor([0, 0, 0.1]).\
         perturb_walls([0.2, 0.2, 0.01])
 
@@ -42,7 +42,7 @@ def synthetic_small_line_scene():
     camera_interval = 3
     position_perturbation = [0.2, 0.2, 0.01]
     rotation_perturbation = 0.3
-    camera = get_camera('perspective', '1', 0.9, -0.1, 0.01)
+    camera = ss.get_camera('perspective', '1', 0.9, -0.1, 0.01)
     scene.add_camera_sequence(camera, 0, scene_length,
                               camera_height, camera_interval,
                               position_perturbation,
