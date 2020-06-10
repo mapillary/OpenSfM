29349e46c28d69ed111c9ba2ab2146a4f403a254
Fabian Schenk <fabianschenk@mapillary.com>
2020-06-10 09:40:32 +0200

    fix: set distortion instead of principal_point

--- opensfm/src/geometry/python/pybind.cc
+++ opensfm/src/geometry/python/pybind.cc
@@ -147,9 +147,9 @@ PYBIND11_MODULE(pygeometry, m) {
               Vec2d principal_point = Vec2d::Zero();
               principal_point << values.at(Camera::Parameters::Cx), values.at(Camera::Parameters::Cy);
               VecXd distortion(5);
-              principal_point << values.at(Camera::Parameters::K1), values.at(Camera::Parameters::K2),
-                                  values.at(Camera::Parameters::K3), values.at(Camera::Parameters::P1),
-                                  values.at(Camera::Parameters::P2);
+              distortion << values.at(Camera::Parameters::K1), values.at(Camera::Parameters::K2),
+                            values.at(Camera::Parameters::K3), values.at(Camera::Parameters::P1),
+                            values.at(Camera::Parameters::P2);
               camera = Camera::CreateBrownCamera(values.at(Camera::Parameters::Focal),
                                                   values.at(Camera::Parameters::AspectRatio),
                                                   principal_point, distortion);
