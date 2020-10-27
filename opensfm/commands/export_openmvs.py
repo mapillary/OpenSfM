c965d58bf19dd12d61412feaeec08c79dfcd0381
Piero Toffanin <pt@masseranolabs.com>
2020-10-27 20:56:06 +0000

    Support for --image_list in export openmvs

--- opensfm/commands/export_openmvs.py
+++ opensfm/commands/export_openmvs.py
@@ -7,7 +7,10 @@ class Command(command.CommandBase):
     help = "Export reconstruction to openMVS format"
 
     def run_impl(self, dataset, args):
-        export_openmvs.run_dataset(dataset)
+        export_openmvs.run_dataset(dataset, args.image_list)
 
     def add_arguments_impl(self, parser):
-        pass
+        parser.add_argument('--image_list',
+                            type=str,
+                            help='Export only the shots included in this file (path to .txt file)')
+
