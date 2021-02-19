8bd8946c348e35c1db941099750597da8f72658d
Fabian Schenk <fschenk@fb.com>
2021-02-19 03:14:46 -0800

    Add dataset type parameter to command_runner
    Summary:
    - Add `--dataset-type` parameter to be able to choose a dataset from the command line.
    - The default value is `opensfm`
    
    Reviewed By: YanNoun, paulinus
    
    Differential Revision: D26311519
    
    fbshipit-source-id: 7856b59008ca3e1bce9301408eb02cd95ed53eda

--- opensfm/commands/command_runner.py
+++ opensfm/commands/command_runner.py
@@ -3,7 +3,7 @@ import argparse
 from opensfm import log
 
 
-def command_runner(all_commands_types, dataset_factory):
+def command_runner(all_commands_types, dataset_factory, dataset_choices):
     """ Main entry point for running the passed SfM commands types."""
     log.setup()
 
@@ -18,12 +18,19 @@ def command_runner(all_commands_types, dataset_factory):
     for command in command_objects:
         subparser = subparsers.add_parser(command.name, help=command.help)
         command.add_arguments(subparser)
+        subparser.add_argument(
+            "--dataset-type",
+            type=str,
+            required=False,
+            default="opensfm",
+            choices=dataset_choices,
+        )
 
     # Parse arguments
     args = parser.parse_args()
 
     # Instanciate datast
-    data = dataset_factory(args.dataset)
+    data = dataset_factory(args.dataset, args.dataset_type)
 
     # Run the selected subcommand
     for command in command_objects:
