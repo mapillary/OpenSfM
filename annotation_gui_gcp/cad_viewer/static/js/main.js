1f9193c16d1e907e6a57841b6d0f2e12e87878cf
Manuel López Antequera <mlop@fb.com>
2021-03-10 09:56:40 +0100

    chore: Load JS directly from url, simplify files

--- annotation_gui_gcp/cad_viewer/static/js/main.js
+++ annotation_gui_gcp/cad_viewer/static/js/main.js
@@ -1,8 +1,8 @@
-import * as THREE from 'three';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
-import { FlyControls } from "three/examples/jsm/controls/FlyControls.js";
-import Stats from '../../node_modules/three/examples/jsm/libs/stats.module.js';
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import { OBJLoader } from 'https://unpkg.com/three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'https://unpkg.com/three/examples/jsm/loaders/MTLLoader.js';
+import { FlyControls } from 'https://unpkg.com/three/examples/jsm/controls/FlyControls.js';
+import Stats from 'https://unpkg.com/three/examples/jsm/libs/stats.module.js';
 
 //----Variables----//
 //DOM element to attach the renderer to
@@ -12,29 +12,23 @@ var viewport;
 var controls;
 
 //viewport size
-var viewportWidth = 800;
-var viewportHeight = 600;
+let viewportWidth = 800;
+let viewportHeight = 600;
 
 //camera attributes
-var view_angle = 45,
-    aspect = viewportWidth / viewportHeight,
-    near = 0.1,//near clip-plane
-    far = 10000;//far clip-plane
+const view_angle = 45;
+const aspect = viewportWidth / viewportHeight;
 
 //----Constructors----//
-var renderer = new THREE.WebGLRenderer({antialias: true});
-var scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const scene = new THREE.Scene();
 let clock = new THREE.Clock();
 
-var camera = new THREE.PerspectiveCamera(
-    view_angle,
-    aspect,
-    near,
-    far
-);
+const camera = new THREE.PerspectiveCamera(view_angle, aspect);
 
 //constructs an instance of a white light
-var pointLight = new THREE.PointLight( 0xFFFFFF );
+renderer.setClearColor(0x006600); // Green background
+const pointLight = new THREE.PointLight(0xFFFFFF);
 
 
 // clicking
@@ -49,43 +43,44 @@ const mouse = new THREE.Vector2();
 const spheres = [];
 const n_spheres = 1;
 const threshold = 0.1;
-const object_scale = 0.001
+// const object_scale = 1.0
 
-{
+function load_cad_model(path_model, path_material) {
+    console.log("Loading CAD model")
     const mtlLoader = new MTLLoader();
-    mtlLoader.load('/static/resources/teapot.mtl' , (mtl) => {
+    mtlLoader.load(path_material, (mtl) => {
 
         mtl.preload();
         const objLoader = new OBJLoader();
         objLoader.setMaterials(mtl);
-        objLoader.load('/static/resources/teapot.obj' , (root) => {
+        objLoader.load(path_model, (root) => {
 
             // Set as true to maintain the consistency of vertex position after scaling
-            for ( let i = 0, l = root.children.length; i < l; i ++ ) {
-                root.children[i].geometry.verticesNeedUpdate = true
-            }
+            // for (let i = 0, l = root.children.length; i < l; i++) {
+            //     root.children[i].geometry.verticesNeedUpdate = true
+            // }
 
-            root.scale.set(object_scale, object_scale, object_scale);
+            // root.scale.set(object_scale, object_scale, object_scale);
             scene.add(root);
 
             // add points
-            for ( let i = 0, l = root.children.length; i < l; i ++ ) {
+            for (let i = 0, l = root.children.length; i < l; i++) {
                 let material = new THREE.PointsMaterial({ color: 0xFFFFFF, size: 0.25 })
                 let mesh = new THREE.Points(root.children[i].geometry, material)
-                mesh.scale.set(object_scale, object_scale, object_scale);
+                //mesh.scale.set(object_scale, object_scale, object_scale);
                 pointclouds.push(mesh);
                 scene.add(mesh)
             }
 
-            // Set the camera position to object center
-            var i = root.children.length;
-            var geometry = root.children[i-1].geometry;
-            geometry.computeBoundingBox();
-            var center = new THREE.Vector3();
-            geometry.boundingBox.getCenter( center );
-            root.children[i-1].localToWorld( center );
-            center = center.multiplyScalar(object_scale)
-            camera.position.copy(center);
+            // // Set the camera position to object center
+            // const i = root.children.length;
+            // const geometry = root.children[i - 1].geometry;
+            // geometry.computeBoundingBox();
+            // let center = new THREE.Vector3();
+            // geometry.boundingBox.getCenter(center);
+            // root.children[i - 1].localToWorld(center);
+            // center = center.multiplyScalar(object_scale)
+            // camera.position.copy(center);
         });
     });
 }
@@ -93,89 +88,91 @@ const object_scale = 0.001
 
 //a cross-browser method for efficient animation, more info at:
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            window.oRequestAnimationFrame      ||
-            window.msRequestAnimationFrame     ||
-            function( callback ){
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
             window.setTimeout(callback, 1000 / 60);
-            };
+        };
 })();
 
-//----Initialization----//
-function initialize()
-{
+function setup_scene() {
     //Sets up the renderer to the same size as a DOM element
     //and attaches it to that element
     renderer.setSize(viewportWidth, viewportHeight);
     viewport = document.getElementById('viewport');
     viewport.appendChild(renderer.domElement);
 
-    camera.position.set(0,0,200);
+    camera.position.set(10, 10, 10);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     //attaches fly controls to the camera
     controls = new FlyControls(camera, renderer.domElement);
-
-    //camera control properties
     controls.movementSpeed = 1;
     controls.domElement = viewport;
     controls.rollSpeed = 0.01;
     controls.autoForward = false;
     controls.dragToLook = true;
 
-
-    pointLight.position.set(10,50,150);
-
-    //add the objects to the scene
-    scene.add(camera);
-    scene.add(pointLight);
-
-    const sphereGeometry = new THREE.SphereBufferGeometry( 0.1, 32, 32 );
-    const sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    for ( let i = 0; i < n_spheres; i ++ ) {
-        const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-        scene.add( sphere );
-        spheres.push( sphere );
-    }
-
-    //
+    pointLight.position.set(10, 50, 150);
 
     raycaster = new THREE.Raycaster();
     raycaster.params.Points.threshold = threshold;
 
-    //
-
     stats = new Stats();
 
-    //
+    scene.add(camera);
+    scene.add(pointLight);
+}
 
-    window.addEventListener( 'resize', onWindowResize, false );
-    document.addEventListener( 'pointerdown', onDocumentMouseClick, false );
+function initialize() {
+    setup_scene();
+
+
+    // Add a sphere that we'll use as a marker
+    const sphereGeometry = new THREE.SphereBufferGeometry(0.1, 32, 32);
+    const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    for (let i = 0; i < n_spheres; i++) {
+        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+        scene.add(sphere);
+        spheres.push(sphere);
+    }
+
+    // Add the model
+    const path_model = "/static/resources/teapot.obj";
+    const path_material = "/static/resources/teapot.mtl";
+    load_cad_model(path_model, path_material)
+
+
+
+    window.addEventListener('resize', onWindowResize, false);
+    // document.addEventListener( 'pointerdown', onDocumentMouseClick, false );
 
     // call update
     update();
 }
 
 
-function onDocumentMouseClick( event ) {
+function onDocumentMouseClick(event) {
 
     event.preventDefault();
 
-    switch ( event.button ) {
+    switch (event.button) {
         case 0: // left
-            var pickposition = setPickPosition(event)
+            const pickposition = setPickPosition(event)
             mouse.x = pickposition.x;
             mouse.y = pickposition.y;
 
 
-            raycaster.setFromCamera( mouse, camera );
+            raycaster.setFromCamera(mouse, camera);
 
-            const intersections = raycaster.intersectObjects( pointclouds, true );
-            intersection = ( intersections.length ) > 0 ? intersections[ 0 ] : null;
+            const intersections = raycaster.intersectObjects(pointclouds, true);
+            intersection = (intersections.length) > 0 ? intersections[0] : null;
 
-            if ( toggle > 0.02 && intersection !== null ) {
+            if (toggle > 0.02 && intersection !== null) {
 
                 console.log("Select point:");
                 console.log(intersection.point);
@@ -187,8 +184,8 @@ function onDocumentMouseClick( event ) {
                     'Content-Type': 'application/json',
                 };
 
-                var data = {
-                    xyz: [ intersection.point['x'], intersection.point['y'], intersection.point['z'] ],
+                const data = {
+                    xyz: [intersection.point['x'], intersection.point['y'], intersection.point['z']],
                     command: "add_or_update_point_observation",
                 };
                 const body = JSON.stringify(data, null, 4);
@@ -197,18 +194,18 @@ function onDocumentMouseClick( event ) {
                 const response = fetch(url, { method, headers, body })
 
 
-                spheres[ spheresIndex ].position.copy( intersection.point );
-                spheres[ spheresIndex ].scale.set( 1, 1, 1 );
-                spheresIndex = ( spheresIndex + 1 ) % spheres.length;
+                spheres[spheresIndex].position.copy(intersection.point);
+                spheres[spheresIndex].scale.set(1, 1, 1);
+                spheresIndex = (spheresIndex + 1) % spheres.length;
 
                 toggle = 0;
             }
 
-            for ( let i = 0; i < spheres.length; i ++ ) {
+            for (let i = 0; i < spheres.length; i++) {
 
-                const sphere = spheres[ i ];
-                sphere.scale.multiplyScalar( 0.98 );
-                sphere.scale.clampScalar( 0.01, 1 );
+                const sphere = spheres[i];
+                sphere.scale.multiplyScalar(0.98);
+                sphere.scale.clampScalar(0.01, 1);
 
             }
             break;
@@ -221,7 +218,7 @@ function onDocumentMouseClick( event ) {
                 'Content-Type': 'application/json',
             };
 
-            var data = {
+            const data = {
                 command: "remove_point_observation",
             };
             const body = JSON.stringify(data, null, 4);
@@ -236,17 +233,17 @@ function onDocumentMouseClick( event ) {
 function getCanvasRelativePosition(event) {
     const rect = viewport.getBoundingClientRect();
     return {
-      x: (event.clientX - rect.left) * window.innerWidth  / rect.width,
-      y: (event.clientY - rect.top ) * window.innerHeight / rect.height,
+        x: (event.clientX - rect.left) * window.innerWidth / rect.width,
+        y: (event.clientY - rect.top) * window.innerHeight / rect.height,
     };
-  }
+}
 
 function setPickPosition(event) {
-const pos = getCanvasRelativePosition(event);
-return {
-    x: (pos.x / window.innerWidth ) *  2 - 1,
-    y: (pos.y / window.innerHeight) * -2 + 1,  // note we flip Y
-};
+    const pos = getCanvasRelativePosition(event);
+    return {
+        x: (pos.x / window.innerWidth) * 2 - 1,
+        y: (pos.y / window.innerHeight) * -2 + 1,  // note we flip Y
+    };
 }
 
 function onWindowResize() {
@@ -254,17 +251,17 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
 
 //----Update----//
 function update() {
     //requests the browser to call update at it's own pace
-    requestAnimFrame( update );
+    requestAnimFrame(update);
 
     //update controls
-    controls.update( 1 );
+    controls.update(1);
 
     //call draw
     draw();
