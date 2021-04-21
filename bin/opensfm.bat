de4a7fe3825a02a778bddf9560ac4624b5465190
Piero Toffanin <pt@masseranolabs.com>
2021-04-21 15:27:10 -0400

    Don't set OPENCV_LIB (Windows)

--- bin/opensfm.bat
+++ bin/opensfm.bat
@@ -2,7 +2,6 @@
 
 setlocal
 set OSFMBASE=%~dp0
-if not defined OPENCV_LIBS set OPENCV_LIBS=%OSFMBASE%\..\vcpkg\installed\x64-windows\bin
 
 python "%OSFMBASE%\opensfm_main.py" %*
 
--- opensfm/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-if sys.platform == 'win32':
-    # Python modules require the OpenCV DLLs
-    # but these might be in a different path
-    if os.environ.get("OPENCV_LIBS"):
-        os.add_dll_directory(os.environ.get("OPENCV_LIBS"))
