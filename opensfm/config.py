95dd9ec16d0ee874c8f0bc827d832b817b07c935
Yann Noutary <ynoutary@fb.com>
2021-07-13 10:09:44 +0200

    fix: restore no-GCP by default

--- opensfm/config.py
+++ opensfm/config.py
@@ -131,7 +131,7 @@ use_altitude_tag: no                  # Use or ignore EXIF altitude tag
 align_method: orientation_prior       # orientation_prior or naive
 align_orientation_prior: horizontal   # horizontal, vertical or no_roll
 bundle_use_gps: yes                   # Enforce GPS position in bundle adjustment
-bundle_use_gcp: yes                   # Enforce Ground Control Point position in bundle adjustment
+bundle_use_gcp: no                    # Enforce Ground Control Point position in bundle adjustment
 bundle_compensate_gps_bias: yes       # Compensate GPS with a per-camera similarity transform
 
 
