a81ce322edc288e64e2e9e0fb92f4379dadb8b12
Fabian Schenk <fschenk@fb.com>
2021-09-27 09:42:52 -0700

    Type annotations for types.py
    Summary: Add type annotations for types.py
    
    Reviewed By: mlopezantequera
    
    Differential Revision: D31170044
    
    fbshipit-source-id: 0ebf5336d6b42777e2bb8e3957de20b8a93ff128

--- opensfm/types.py
+++ opensfm/types.py
@@ -1,4 +1,7 @@
 """Basic types for building a reconstruction."""
+from typing import Dict, Optional
+
+import numpy as np
 from opensfm import pygeometry
 from opensfm import pymap
 from opensfm.geo import TopocentricConverter
@@ -27,7 +30,7 @@ class Reconstruction(object):
       reference (TopocentricConverter): Topocentric reference converter.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Defaut constructor"""
         self.map = pymap.Map()
         self.camera_view = pymap.CameraView(self.map)
@@ -38,72 +41,72 @@ class Reconstruction(object):
         self.pano_shot_view = pymap.PanoShotView(self.map)
         self.landmark_view = pymap.LandmarkView(self.map)
 
-    def get_cameras(self):
+    def get_cameras(self) -> pymap.CameraView:
         return self.camera_view
 
-    def set_cameras(self, value):
+    def set_cameras(self, value: Dict[str, pygeometry.Camera]) -> None:
         for cam in value.values():
             self.map.create_camera(cam)
 
     cameras = property(get_cameras, set_cameras)
 
-    def get_biases(self):
+    def get_biases(self) -> pymap.BiasView:
         return self.bias_view
 
-    def set_biases(self, value):
+    def set_biases(self, value: Dict[str, pygeometry.Similarity]) -> None:
         for cam_id, bias in value.items():
             self.map.set_bias(cam_id, bias)
 
-    def set_bias(self, cam_id, bias):
+    def set_bias(self, cam_id: str, bias: pygeometry.Similarity) -> None:
         self.map.set_bias(cam_id, bias)
 
     biases = property(get_biases, set_biases)
 
-    def get_rig_cameras(self):
+    def get_rig_cameras(self) -> pymap.RigCameraView:
         return self.rig_cameras_view
 
-    def set_rig_cameras(self, values):
+    def set_rig_cameras(self, values: Dict[str, pymap.RigCamera]) -> None:
         for rig_camera in values.values():
             self.map.create_rig_camera(rig_camera)
 
     rig_cameras = property(get_rig_cameras, set_rig_cameras)
 
-    def get_rig_instances(self):
+    def get_rig_instances(self) -> pymap.RigInstanceView:
         return self.rig_instances_view
 
-    def set_rig_instances(self, values):
-        for rig_instance in values:
+    def set_rig_instances(self, values: Dict[str, pymap.RigInstance]) -> None:
+        for rig_instance in values.values():
             self.add_rig_instance(rig_instance)
 
     rig_instances = property(get_rig_instances, set_rig_instances)
 
-    def get_shots(self):
+    def get_shots(self) -> pymap.ShotView:
         return self.shot_view
 
-    def set_shots(self, value):
+    def set_shots(self, value: Dict[str, pymap.Shot]) -> None:
         for shot in value.values():
             self.add_shot(shot)
 
     shots = property(get_shots, set_shots)
 
-    def get_pano_shots(self):
+    def get_pano_shots(self) -> pymap.PanoShotView:
         return self.pano_shot_view
 
-    def set_pano_shots(self, value):
+    def set_pano_shots(self, value: Dict[str, pymap.Shot]) -> None:
         for shot in value.values():
             self.add_pano_shot(shot)
 
     pano_shots = property(get_pano_shots, set_pano_shots)
 
-    def get_points(self):
+    def get_points(self) -> pymap.LandmarkView:
         return self.landmark_view
 
-    def set_points(self, value):
+    def set_points(self, value: Dict[str, pymap.Landmark]) -> None:
         self.map.clear_observations_and_landmarks()
         for point in value.values():
             self.add_point(point)
 
-    def remove_point(self, point_id):
+    def remove_point(self, point_id: str) -> None:
         self.map.remove_landmark(point_id)
 
     points = property(get_points, set_points)
@@ -118,14 +121,14 @@ class Reconstruction(object):
     reference = property(get_reference, set_reference)
 
     # Cameras
-    def add_camera(self, camera):
+    def add_camera(self, camera: pygeometry.Camera) -> pygeometry.Camera:
         """Add a camera in the list
 
         :param camera: The camera.
         """
         return self.map.create_camera(camera)
 
-    def get_camera(self, id):
+    def get_camera(self, id: str) -> pygeometry.Camera:
         """Return a camera by id.
 
         :return: If exists returns the camera, otherwise None.
@@ -133,14 +136,14 @@ class Reconstruction(object):
         return self.cameras.get(id)
 
     # Rigs
-    def add_rig_camera(self, rig_camera):
+    def add_rig_camera(self, rig_camera: pymap.RigCamera) -> pymap.RigCamera:
         """Add a rig camera in the list
 
         :param rig_camera: The rig camera.
         """
         return self.map.create_rig_camera(rig_camera)
 
-    def add_rig_instance(self, rig_instance):
+    def add_rig_instance(self, rig_instance: pymap.RigInstance) -> pymap.RigInstance:
         """Creates a copy of the passed rig instance
         in the current reconstruction"""
 
@@ -159,12 +162,14 @@ class Reconstruction(object):
         return self.map.update_rig_instance(rig_instance)
 
     # Shot
-    def create_shot(self, shot_id, camera_id, pose=None):
+    def create_shot(
+        self, shot_id: str, camera_id: str, pose: Optional[pygeometry.Pose] = None
+    ) -> pymap.Shot:
         if pose is None:
             pose = pygeometry.Pose()
         return self.map.create_shot(shot_id, camera_id, pose)
 
-    def add_shot(self, shot):
+    def add_shot(self, shot: pymap.Shot) -> pymap.Shot:
         """Creates a copy of the passed shot
         in the current reconstruction"""
 
@@ -174,63 +179,71 @@ class Reconstruction(object):
             self.create_shot(shot.id, shot.camera.id, shot.pose)
         return self.map.update_shot(shot)
 
-    def get_shot(self, id):
+    def get_shot(self, id: str) -> pymap.Shot:
         """Return a shot by id.
 
         :return: If exists returns the shot, otherwise None.
         """
         return self.shots.get(id)
 
-    def remove_shot(self, shot_id):
+    def remove_shot(self, shot_id: str) -> None:
         self.map.remove_shot(shot_id)
 
     # PanoShot
-    def create_pano_shot(self, shot_id, camera_id, pose=None):
+    def create_pano_shot(
+        self, shot_id: str, camera_id: str, pose: Optional[pygeometry.Pose] = None
+    ) -> pymap.Shot:
         if pose is None:
             pose = pygeometry.Pose()
         return self.map.create_pano_shot(shot_id, camera_id, pose)
 
-    def add_pano_shot(self, pshot):
+    def add_pano_shot(self, pshot: pymap.Shot) -> pymap.Shot:
         if pshot.camera.id not in self.cameras:
             self.add_camera(pshot.camera)
         if pshot.id not in self.pano_shots:
             self.create_pano_shot(pshot.id, pshot.camera.id, pshot.pose)
         return self.map.update_pano_shot(pshot)
 
-    def get_pano_shot(self, id):
+    def get_pano_shot(self, id: str) -> pymap.Shot:
         """Return a shot by id.
 
         :return: If exists returns the shot, otherwise None.
         """
         return self.pano_shots.get(id)
 
-    def remove_pano_shot(self, shot_id):
+    def remove_pano_shot(self, shot_id: str) -> None:
         self.map.remove_pano_shot(shot_id)
 
-    def create_point(self, point_id, coord=(0, 0, 0)):
+    def create_point(
+        self, point_id: str, coord: Optional[np.ndarray] = None
+    ) -> pymap.Landmark:
+        if coord is None:
+            return self.map.create_landmark(point_id, np.array([0, 0, 0]))
         return self.map.create_landmark(point_id, coord)
 
-    def add_point(self, point):
+    def add_point(self, point: pymap.Landmark) -> pymap.Landmark:
         """Add a point in the list
 
         :param point: The point.
         """
         if point.coordinates is None:
-            new_pt = self.map.create_landmark(point.id, [0, 0, 0])
+            new_pt = self.map.create_landmark(point.id, np.array([0, 0, 0]))
         else:
             new_pt = self.map.create_landmark(point.id, point.coordinates)
         if point.color is not None:
             new_pt.color = point.color
         return new_pt
 
-    def get_point(self, id):
+    def get_point(self, id: str) -> pymap.Landmark:
         """Return a point by id.
 
         :return: If exists returns the point, otherwise None.
         """
         return self.points.get(id)
 
-    def add_observation(self, shot_id, lm_id, observation):
+    def add_observation(
+        self, shot_id: str, lm_id: str, observation: pymap.Observation
+    ) -> None:
         """Adds an observation between a shot and a landmark
         :param shot_id: The id of the shot
         :param lm_id: The id of the landmark
@@ -238,7 +251,7 @@ class Reconstruction(object):
         """
         self.map.add_observation(shot_id, lm_id, observation)
 
-    def remove_observation(self, shot_id, lm_id):
+    def remove_observation(self, shot_id: str, lm_id: str) -> None:
         self.map.remove_observation(shot_id, lm_id)
 
     def __deepcopy__(self, d):
@@ -270,7 +283,9 @@ class Reconstruction(object):
 
         return rec_cpy
 
-    def add_correspondences_from_tracks_manager(self, tracks_manager):
+    def add_correspondences_from_tracks_manager(
+        self, tracks_manager: pymap.TracksManager
+    ) -> None:
         for track_id in self.points.keys():
             track_obs = tracks_manager.get_track_observations(track_id)
             for shot_id in track_obs.keys():
