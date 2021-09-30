19e0423ed34c20b060fd7dbfcf6f1db4e4f3a76b
Fabian Schenk <fschenk@fb.com>
2021-09-30 04:08:43 -0700

    Fix undefined variable in reconstruct_from_prior
    Summary: - Mentioned in this [issue](https://github.com/mapillary/OpenSfM/issues/792#issuecomment-929224637)
    
    Reviewed By: mwerlberger
    
    Differential Revision: D31236337
    
    fbshipit-source-id: daab498411a3094516963ec65ea48b53d769c004

--- opensfm/actions/reconstruct_from_prior.py
+++ opensfm/actions/reconstruct_from_prior.py
@@ -11,7 +11,5 @@ def run_dataset(data: DataSetBase, input: str, output: str):
         report, rec = reconstruction.reconstruct_from_prior(
             data, tracks_manager, rec_prior[0]
         )
-    # pyre-fixme[61]: `rec` may not be initialized here.
-    data.save_reconstruction([rec], output)
-    # pyre-fixme[61]: `report` may not be initialized here.
-    data.save_report(io.json_dumps(report), "reconstruction.json")
+        data.save_reconstruction([rec], output)
+        data.save_report(io.json_dumps(report), "reconstruction.json")
