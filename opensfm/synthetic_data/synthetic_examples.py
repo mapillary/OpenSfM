b44b5f2b533b6fce8055b3a5a98a59bc22ae2cf6
Yann Noutary <ynoutary@fb.com>
2022-01-28 04:18:25 -0800

    fix: handling of rig cameras in merging
    Reviewed By: paulinus
    
    Differential Revision: D33660489
    
    fbshipit-source-id: 5da48313f7b15c636ada18acc1e9200b6b438448

--- opensfm/synthetic_data/synthetic_examples.py
+++ opensfm/synthetic_data/synthetic_examples.py
@@ -5,8 +5,8 @@ from opensfm import geo
 
 
 def synthetic_circle_scene(
-    reference: Optional[geo.TopocentricConverter] = None, with_panoshot: bool = False
-):
+    reference: Optional[geo.TopocentricConverter] = None,
+) -> ss.SyntheticStreetScene:
     scene_length = 60
     points_count = 5000
     generator = ss.get_scene_generator("circle", scene_length)
@@ -15,53 +15,17 @@ def synthetic_circle_scene(
         [0.2, 0.2, 0.01]
     )
 
-    # regular sequence
-    camera_height = 1.5
-    camera_interval = 3
-    position_perturbation = [0.2, 0.2, 0.01]
-    rotation_perturbation = 0.2
-    camera1 = ss.get_camera("perspective", "1", 0.7, -0.1, 0.01)
-    scene.add_camera_sequence(
-        camera1,
-        scene_length,
-        camera_height,
-        camera_interval,
-        position_perturbation,
-        rotation_perturbation,
-        None,
-    )
-
-    # panoshot sequence
-    if with_panoshot:
-        panoshot_size = 1e-6
-        panoshot_count = 6
-        panoshot_interval = panoshot_size / panoshot_count
-        position_perturbation = [
-            panoshot_interval,
-            panoshot_interval,
-            panoshot_interval,
-        ]
-        rotation_perturbation = 3.14
-        camera2 = ss.get_camera("perspective", "2", 0.5, -0.1, 0.01)
-        scene.add_camera_sequence(
-            camera2,
-            1.0,
-            camera_height,
-            panoshot_interval,
-            position_perturbation,
-            rotation_perturbation,
-            None,
-            panoshot_size,
-        )
-
+    make_regular_scene(scene_length, scene)
     return scene
 
 
-def synthetic_cube_scene():
+def synthetic_cube_scene() -> ss.SyntheticCubeScene:
     return ss.SyntheticCubeScene(10, 1000, 0.001)
 
 
-def synthetic_rig_scene(reference: Optional[geo.TopocentricConverter] = None):
+def synthetic_rig_scene(
+    reference: Optional[geo.TopocentricConverter] = None,
+) -> ss.SyntheticStreetScene:
     scene_length = 20
     points_count = 5000
     generator = ss.get_scene_generator("line", scene_length)
@@ -69,7 +33,11 @@ def synthetic_rig_scene(reference: Optional[geo.TopocentricConverter] = None):
     scene.add_street(points_count, 15, 12).perturb_floor([0, 0, 0.1]).perturb_walls(
         [0.2, 0.2, 0.01]
     )
+    make_rig_scene(scene_length, scene)
+    return scene
 
+
+def make_rig_scene(scene_length: float, scene: ss.SyntheticStreetScene) -> None:
     camera_height = 2
     camera_interval = 3
     position_perturbation = [0.2, 0.2, 0.01]
@@ -100,4 +68,20 @@ def synthetic_rig_scene(reference: Optional[geo.TopocentricConverter] = None):
         position_perturbation,
         rotation_perturbation,
     )
-    return scene
+
+
+def make_regular_scene(scene_length: float, scene: ss.SyntheticStreetScene) -> None:
+    camera_height = 1.5
+    camera_interval = 3
+    position_perturbation = [0.2, 0.2, 0.01]
+    rotation_perturbation = 0.2
+    camera1 = ss.get_camera("perspective", "1", 0.7, -0.1, 0.01)
+    scene.add_camera_sequence(
+        camera1,
+        scene_length,
+        camera_height,
+        camera_interval,
+        position_perturbation,
+        rotation_perturbation,
+        None,
+    )
