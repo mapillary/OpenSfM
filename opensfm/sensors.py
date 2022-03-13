32e73fca4fd7672cd00ccb7b052e66b2fdf98946
zfb132 <zfb132@gmail.com>
2022-03-13 21:59:30 +0800

    fix no context.SENSOR error

--- opensfm/sensors.py
+++ opensfm/sensors.py
@@ -7,7 +7,7 @@ from opensfm import io
 
 @lru_cache(1)
 def sensor_data():
-    with io.open_rt(context.SENSOR) as f:
+    with io.open_rt(context.SENSOR_DATA) as f:
         data = io.json_load(f)
 
     # Convert model types to lower cases for easier query
