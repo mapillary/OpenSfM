fix: use rotation in case of missing GPS/GCPs
Summary: This Diff loosen the alignment procedure with orientation priors, by relaxing the need of GPS/GCPs : in that case, we can still use the rotation for alignment (identity scale and translation).

Reviewed By: fabianschenk

Differential Revision: D36478909

fbshipit-source-id: 3a679a75cb15b0da0bbeda3b20f2993f658ff6fb

opensfm/align.py
@@ -250,19 +250,19 @@ def compute_orientation_prior_similarity(
      - horizontal: assumes cameras are looking towards the horizon
      - vertical: assumes cameras are looking down towards the ground
     """
-    X, Xp = alignment_constraints(config, reconstruction, gcp, use_gps)
-    X = np.array(X)
-    Xp = np.array(Xp)
-
-    if len(X) < 1:
-        return None
-
     p = estimate_ground_plane(reconstruction, config)
     if p is None:
         return None
     Rplane = multiview.plane_horizontalling_rotation(p)
     if Rplane is None:
         return None
+
+    X, Xp = alignment_constraints(config, reconstruction, gcp, use_gps)
+    X = np.array(X)
+    Xp = np.array(Xp)
+    if len(X) < 1:
+        return 1.0, Rplane, np.zeros(3)
+
     X = Rplane.dot(X.T).T
 
     # Estimate 2d similarity to align to GPS