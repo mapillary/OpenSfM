193818dcd274fbc5763c654b0446df0aca696544
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2023-07-31 18:13:07 -0700

    suppress errors in `mapillary/opensfm`
    Differential Revision: D47940244
    
    fbshipit-source-id: dd8961029435e5c43816cbe63b6a01cdc1b52986

--- viewer/server.py
+++ viewer/server.py
@@ -23,6 +23,8 @@ datapath = None
 
 @app.route("/")
 def index() -> Response:
+    # pyre-fixme[6]: For 1st argument expected `typing_extensions.LiteralString` but
+    #  got `Optional[str]`.
     return send_file(os.path.join(app.static_folder, "index.html"))
 
 
@@ -83,7 +85,6 @@ def verified_send(file) -> Response:
     if os.path.isfile(file):
         return send_file(file)
     else:
-        # pyre-fixme[7]: Expected `Response` but got implicit return value of `None`.
         abort(404)
 
 
