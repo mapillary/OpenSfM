4644cd636aefebd414b119628434a2fa64ee2a37
Fabian Schenk <fschenk@meta.com>
2026-06-12 01:36:05 -0700

    const in defines HashPair
    Differential Revision: D108284676
    
    fbshipit-source-id: d5a359d8e889fcd7afeaf6d9924dc30034c1caa4

--- opensfm/src/map/defines.h
+++ opensfm/src/map/defines.h
@@ -36,8 +36,8 @@ using DescriptorMatrix =
 struct HashPair {
   template <class T1, class T2>
   size_t operator()(const std::pair<T1, T2>& p) const {
-    auto hash1 = std::hash<T1>{}(p.first);
-    auto hash2 = std::hash<T2>{}(p.second);
+    const auto hash1 = std::hash<T1>{}(p.first);
+    const auto hash2 = std::hash<T2>{}(p.second);
     return hash1 ^ hash2;
   }
 };
